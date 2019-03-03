7a73ae87ef372efb4193d27e36e41e6db65d993d
Mikedeath <30758577+Mikedeath@users.noreply.github.com>
2019-03-02 23:56:39 -0600

    Add files via upload

---
+++ src/mydata.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+class MyData extends Component{
+
+    
+    constructor(props){
+        super(props);
+
+       
+    }
+
+    SimpleTable= ()=> {
+
+        const styles = theme => ({
+            root: {
+              width: '100%',
+              marginTop: theme.spacing.unit * 5,
+              overflowX: 'auto',
+            },
+            table: {
+              minWidth: 500,
+            },
+          });
+          
+          let id = 0;
+          function createData(name, parcial1, parcial2) {
+            id += 1;
+            return { name, parcial1, parcial2 };
+          }
+
+         const rows = [
+            createData(this.state.clase1, this.state.notaP1C1, this.state.notaP2C1),
+            createData(this.state.clase2, this.state.notaP1C2, this.state.notaP2C2),
+            createData(this.state.clase3, this.state.notaP1C3, this.state.notaP2C3),
+          ];
+          
+        return (
+            <div>
+          <Paper >
+            <Table >
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">Nombre de Clase</TableCell>
+                  <TableCell align="right">Parcial 1</TableCell>
+                  <TableCell align="right">Parcial 2</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+              </TableBody>
+            </Table>
+          </Paper>
+          </div>
+        );
+      }
+
+    handleClick = ()=> {
+
+        this.Table();
+    }
+
+    render(){
+
+        return(
+            <div>
+                <TextField   placeholder="Clase 1"> </TextField>    
+                <TextField  placeholder="Parcial 1 Nota"> </TextField> 
+                <TextField  placeholder="Parcial 2 Nota"> </TextField> 
+                <br/>
+                <TextField   placeholder="Clase 2"> </TextField>  
+                <TextField  placeholder="Parcial 1 Nota"> </TextField>       
+                <TextField  placeholder="Parcial 2 Nota"> </TextField> 
+                <br/>
+                <TextField   placeholder="Clase 3"> </TextField>    
+                <TextField  placeholder="Parcial 1 Nota"> </TextField> 
+                <TextField  placeholder="Parcial 2 Nota"> </TextField> 
+                <br/>
+                <br/>
+                <Button variant="contained" color="primary" onClick={ this.handleClick}>
+                    Guardar
+                </Button>
+
+            
+
+            </div>    
+        );
+    }
+
+}
+
+export default MyData;
+
+
+
